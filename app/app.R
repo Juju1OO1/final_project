ed8b6b5be6905bb4086e4e50b318b31e6f88ab12
Li Zhi-Yun <61776077+Juju1OO1@users.noreply.github.com>
2021-07-12 15:28:13 +0800

    create

---
+++ app/app.R
@@ -0,0 +1,22 @@
+# makecondition -----------
+
+library(shiny)
+library(ggplot2)
+
+
+
+# server --------
+
+server <- function(input, output) {
+  output$distPlot <- shiny::renderPlot({
+    ggplot(data = faithful) +
+      geom_histogram(aes(x = eruptions),
+        bins = as.numeric(input$bins)
+      )
+  })
+}
+
+shinyApp(
+  ui = shiny::htmlTemplate("www/index.html"),
+  server
+)
---
+++ docs/lib/myown-1.0.0/js/myscript.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var el_tabs = document.querySelector(".tabs");
+    var instance_tabs = M.Tabs.init(el_tabs);
+    
+    
+});
+
+$(document).ready(
+  function(){
+    $('.tooltipped').tooltip();}
+    
+);
